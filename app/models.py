b81241821628e8093d113aeafcf3478cf97073d5
jboyd8 <jamieboyd8@yahoo.com>
2019-11-11 15:24:41 +0000

    feat: added unittests

--- app/models.py
+++ app/models.py
@@ -50,11 +50,14 @@ class User(UserMixin, db.Model):
 
     def followed_posts(self):
         followed = Post.query.join(
-            followers, (followers.c.followed_id == Post.user.id)).filter(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -66,6 +69,3 @@ class Post(db.Model):
         return '<Post {}>'.format(self.body)
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
